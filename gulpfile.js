7cc25ab747b8f34bdb5df40c0ab58186a626f53e
Katsuhkay <alex_magalhaes13@hotmail.com>
2025-06-23 20:20:57 -0300

    finalizado com uglify!

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
 
 function styles() {
     return gulp.src('src/styles/*.scss')
@@ -16,10 +23,11 @@ function copyImages() {
 function watchFiles() {
     gulp.watch('src/styles/*.scss', styles);
     gulp.watch('src/images/**/*', copyImages);
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
 
 // Exportando as tarefas
 exports.styles = styles;
 exports.images = copyImages;
 exports.watch = watchFiles;
-exports.default = gulp.parallel(styles, copyImages);
+exports.default = gulp.parallel(styles, copyImages, scripts);
