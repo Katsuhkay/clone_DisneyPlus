79890fba0e19acf6cafc1ed5e9e463972e8e5691
Katsuhkay <alex_magalhaes13@hotmail.com>
2025-06-23 21:16:08 -0300

    tentando resolver deploy

--- gulpfile.js
+++ gulpfile.js
@@ -5,29 +5,28 @@ const uglify = require('gulp-uglify');
 function scripts() {
     return gulp.src('./src/scripts/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('./dist/js'))
+        .pipe(gulp.dest('./public/js')) // Alterado para public
 }
 
 function styles() {
     return gulp.src('src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('public/css')); // Alterado para public
 }
 
 function copyImages() {
     return gulp.src('src/images/**/*')
-        .pipe(gulp.dest('dist/images'));
+        .pipe(gulp.dest('public/images')); // Alterado para public
 }
 
-// Tarefa watch modificada
 function watchFiles() {
     gulp.watch('src/styles/*.scss', styles);
     gulp.watch('src/images/**/*', copyImages);
-    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
+    gulp.watch('./src/scripts/*.js', scripts);
 }
 
-// Exportando as tarefas
 exports.styles = styles;
+exports.scripts = scripts;
 exports.images = copyImages;
 exports.watch = watchFiles;
 exports.default = gulp.parallel(styles, copyImages, scripts);
